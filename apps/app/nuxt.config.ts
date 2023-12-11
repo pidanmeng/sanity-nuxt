e7387fd6bb1c8f5d8e56d8a4fa64742409c9c0f3
mengjingqiu <mengjingqiu@umu.com>
2023-12-11 13:42:28 +0800

    feat: 🎸 init

--- apps/app/nuxt.config.ts
+++ apps/app/nuxt.config.ts
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxtjs/sanity",
+    '@nuxtjs/sanity',
     [
-      "@nuxtjs/google-fonts",
+      '@nuxtjs/google-fonts',
       {
         families: {
-          "IBM Plex Mono": [500, 700],
+          'IBM Plex Mono': [500, 700],
           Inter: [500, 700, 800],
-          "PT Serif": [400, 700],
+          'PT Serif': [400, 700],
           download: true,
           inject: true,
         },
@@ -16,15 +16,15 @@ export default defineNuxtConfig({
     ],
   ],
   sanity: {
-    projectId: process.env.NUXT_ENV_SANITY_PROJECT_ID,
-    dataset: process.env.NUXT_ENV_SANITY_DATASET,
+    projectId: 'exjxieex',
+    dataset: 'production',
     useCdn: true, // `false` if you want to ensure fresh data
-    apiVersion: "2021-03-25",
+    apiVersion: '2021-03-25',
   },
   postcss: {
     plugins: {
       autoprefixer: {},
-      "postcss-nested": {},
+      'postcss-nested': {},
     },
   },
 });
